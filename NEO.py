c9def32c9d1f835d1c78823cbe1ed29b577e6b7f
Steven Marimon <stivsitoo17@gmail.com>
2025-07-30 19:16:36 -0500

    feat : Funcion Nombre del usuario

--- NEO.py
+++ NEO.py
@@ -8,3 +8,14 @@ import webbrowser
 assistant_name = "NEO"
 user_name = ""
 
+def NomUs():
+    global nombre_usuario
+    while True:
+        nombre_usuario = input("por favor ingrese el nombre de usuario con el cual me voy a dirigir a usted: ").strip()
+        if nombre_usuario:
+            print(f"Perfecto, gusto en conocerte {nombre_usuario}")
+            break
+        else:
+            print("Por favor, ingrese un nombre válido (no puede estar vacío).")
+
+NomUs()
